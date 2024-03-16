1e378e3374651d9be5e343f6c020eae70112f224
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-16 20:26:03 -0300

    primeiro exemplo metodo includes

---
+++ metodoincludes1.js
@@ -0,0 +1,6 @@
+//criei um array chamdo frutas
+let frutas = ['Maçã', 'Banana', 'Morango'];
+//o metodo includes verifica se um array inclui um determinado elemento.
+let temBanana = frutas.includes('Banana');
+//exibe resultado.
+console.log(temBanana);
