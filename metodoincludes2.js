54ec48f6e5edc1ac529ba1fadbecb2adc69c22e1
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-16 20:30:03 -0300

    segundo exemplo metodo includes

---
+++ metodoincludes2.js
@@ -0,0 +1,6 @@
+//criei um array chamado cor
+let cor = ['roxo', 'amarelo', 'rosa'];
+//o metodo includes verifica se um array inclui um determinado elemento.
+let temroxo = cor.includes('roxo');
+//exibe resultado.
+console.log(temroxo);
