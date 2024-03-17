1754dfb89977e47f03e958c557d1bbe449c7e29c
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-17 11:39:45 -0300

    segundo exemplo do metodo join

---
+++ metodojoin2.js
@@ -0,0 +1,6 @@
+//criei um array com 2 strings separadas por um espaço em branco
+let array = ['Bom dia', 'todos'];
+//o metodo join junta todos os elementos em uma unica string
+let joinedString = array.join(' ');
+//exibe a mensagem "Frase formada" e mostra o resultado
+console.log("Frase formada:", joinedString);
