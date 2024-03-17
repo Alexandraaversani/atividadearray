45270cf1c4e31e540f7e1f357c77a14f5f3b34f6
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-17 11:32:55 -0300

    metodojoin1.js

---
+++ metodojoin1.js
@@ -0,0 +1,6 @@
+//criei um array com 2 strings separadas por um espaço em branco
+let array = ['Olá', 'mundo'];
+//o metodo join junta todos os elementos em uma unica string
+let joinedString = array.join(' ');
+//exibe a mensagem "String após a união" e mostra o resultado
+console.log("String após a união:", joinedString);
