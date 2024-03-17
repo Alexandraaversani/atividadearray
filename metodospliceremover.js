af4c8f68e5b01e573a47de88e745210fb583a8a0
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-17 11:58:19 -0300

    exemplo splice remover

---
+++ metodospliceremover.js
@@ -0,0 +1,7 @@
+//criei um array com varios elementos
+let array = ['a', 'b', 'c', 'd', 'e'];
+//o 2 indica que quero remover o elemento do indice 2
+//o 1 indica que quero remover apenas um elemento a partir do indice 2.
+array.splice(2, 1);
+//mostra o array depois de executar o splices
+console.log(array);
