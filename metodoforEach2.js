80e778b176123d6aadcdcc3541b8b03c5d8da681
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-17 11:23:27 -0300

    segundo exemplo do metodo forEach

---
+++ metodoforEach2.js
@@ -0,0 +1,5 @@
+//criei um array com numeros.
+let array = [7, 8, 9];
+//o metodo forEach percorre todos os elementos de um array, e faz uma função para cada um.
+//usei uma função de seta que recebe o parâmetro item, e exibe esse item no console.log
+array.forEach(item => console.log(item));
