772b6d9ad29ef2d68c10af12aac6bd86c7f0d489
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-17 10:41:03 -0300

    primeiro exemplo do metodo reverse

---
+++ metodoreverse1.js
@@ -0,0 +1,6 @@
+//criei um array chamado array com uma sequencia de numeros.
+let array = [1, 2, 3];
+//metodo do array que inverte a ordem dos elementos de um array.
+array.reverse();
+//o console.log vai exibir a mensagem: array invertido e mostrar a ordem invertida.
+console.log("array invertido:", array);
