e9c36f28af528a91775a9e8991c373ebcd649982
AlexandraAversani <alexandraaversani.tds2.senai@gmail.com>
2024-03-17 11:05:53 -0300

    segundo exemplo do metodo sort

---
+++ metodosort2.js
@@ -0,0 +1,6 @@
+//criei um array chamado array, com numeros fora da sequencia
+let array = [5, 4, 3,];
+//esse é o metodo que ordena os elementos de um array por padrão em ordem alfabetica ou numérica.
+array.sort();
+//o console.log vai exibir a mensagem "array após a ordenação" e mostrar o array.
+console.log("Array após a ordenação:", array);
